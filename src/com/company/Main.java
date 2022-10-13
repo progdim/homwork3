3c30d24727ca060508249768b9e3c921bb55e02e
Admin <dmtr2001@mail.ru>
2022-10-13 20:22:55 +0700

    in 13.10

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int age = 25;
+
+	if (age>=2 & age<=6 ){
+        System.out.println("Go to Детский сад");
+    }
+    if (age>=7 & age <=18){
+        System.out.println("Идем в школу");
+    }
+    if (age>=18 & age <=24){
+        System.out.println("его место в университете");
+        }
+    if (age>24){
+        System.out.println("ему пора ходить на работу");
+        }
     }
 }
