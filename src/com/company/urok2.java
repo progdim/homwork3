bece815aa59295c1168a50b2de9e6532c3158303
Admin <dmtr2001@mail.ru>
2022-10-20 22:11:38 +0700

    in 13.10

---
+++ src/com/company/urok2.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class urok2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+//        System.out.println("Добрый день, подскажите какой у вас телефон? В качествет ответа напишите 0 или 1");
+//        int clientOS = in.nextInt();
+//        System.out.println("Укажите год выпуска вашего устройства");
+//        int deviceYear = in.nextInt();
+//        int clientDeviceYear = 2015;
+//
+//        if (clientOS == 0){
+//            if (clientDeviceYear > deviceYear){
+//                System.out.println("Вам требуется установить облегченную версию");
+//            }
+//            else {
+//                System.out.println("Установите версию приложения для iOS по ссылке:");
+//            }
+//        }
+//        if (clientOS == 1){
+//            if (clientDeviceYear > deviceYear){
+//                System.out.println("Вам требуется установить облегченную версию");
+//            }
+//            else {
+//                System.out.println("Установите версию приложения для Android по ссылке:");
+//            }
+//        }
+
+        int days = 0;
+
+        int deliveryDistance = 15;
+
+
+    }
+}
+
