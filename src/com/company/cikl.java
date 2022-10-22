42d25948514c5a34cec094319b003c4914d77c0e
Admin <dmtr2001@mail.ru>
2022-10-22 21:18:45 +0700

    in 13.10

--- src/com/company/cikl.java
+++ src/com/company/cikl.java
@@ -10,13 +10,44 @@ public class cikl {
 //            }
 
 
-        int salary =29000;
-        int total = 0;
-        for (int i =0; i <12; i++){
-            total = total/12;
-            total = total +salary;
+//        int result = 0;
+//        int salary = 20000;
+//        int i = 0;
+//        int pais = 0;
+//        while (result <= 2459000){
+//            pais = result / 100;
+//            result = result + salary + pais;
+//            i++;
+//            System.out.println("месяц " + i + " сумма накоплений равна " + result + "рублей");
+//        }
 
+//        int i = 1;
+//        while (i <= 10) {
+//            System.out.println(i);
+//            i++;
+//        }
+//        for (int j = 10; j > 0; j--) {
+//            System.out.println(j);
+//        }
+
+        int y = 12000000;
+        int born = y / 1000 * 17;
+        int died = y / 1000 * 8;
+        for (int i = 0; i <= 10; i++) {
+            y += born - died;
+            System.out.println("Год " +  i + " численность населения составляет " + y);
         }
-        System.out.println("Месяц"  + "сумма накоплений равна " + total +  " рублей");
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
